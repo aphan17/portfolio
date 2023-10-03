ec76d039f2e1bf9c129f3c476d189e1695e663de
ashley phan <ashleyphan17@gmail.com>
2023-10-03 14:49:40 -0700

    made changes to template

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://aphan17.github.io/',
+  title: 'AP.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Ashley Phan',
+  role: 'Full Stack Software Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    "I'm a Seattle-based software engineer who embarked on a career transition from healthcare to software development. Through my work in healthcare, I had a strong desire to improve access to quality resources for individuals. However, I realized that I could have an even greater impact by utilizing software to help bridge this gap. In the past year, I have been developing and furthering my skills by self-learning with resources like The Odin Project, taking Udemy courses, and recently completing the Hack Reactor Bootcamp. I'm excited to combine my past experiences with my newly acquired technical skills, aiming to craft inventive solutions and create a significant and positive impact.",
+  resume: '',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/ashleyphan-/',
+    github: 'https://github.com/aphan17',
   },
 }
 
@@ -21,28 +21,26 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'The Bench',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A social media platform connecting gym-goers.',
+    stack: ['React', 'FastAPI', 'Python', 'JavaScript', 'PostgreSQL', 'Docker'],
+    sourceCode: 'https://gitlab.com/macd-may/module3-project-gamma',
+    livePreview: 'https://macd-may.gitlab.io/module3-project-gamma/',
   },
   {
-    name: 'Project 2',
+    name: 'Trail Tales',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A web blog application for local hikers in the Seattle Area',
+    stack: ['HTML', 'CSS', 'Django', 'Python'],
+    sourceCode: 'https://github.com/aphan17/trail-tales',
   },
   {
-    name: 'Project 3',
+    name: 'Flow',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A task manager application designed for project planning and tracking',
+    stack: ['HTML', 'CSS', 'Django', 'Python'],
+    sourceCode: 'https://github.com/aphan17/flow',
   },
 ]
 
@@ -52,20 +50,17 @@ const skills = [
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Python',
+  'Django',
+  'FastAPI'
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'ashleyphan17@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
